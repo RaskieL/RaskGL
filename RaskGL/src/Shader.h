01603e5d272b9779907bc3e85a770d88b42968dd
Maïken <107373121+RaskieL@users.noreply.github.com>
2025-09-07 12:24:14 +0200

    Add project files.

---
+++ RaskGL/src/Shader.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+struct ShaderProgramSource {
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+class Shader {
+private:
+	std::string m_FilePath;
+	unsigned int m_RendererID;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
+public:
+	Shader(const std::string& filepath);
+	~Shader();
+
+	void Bind() const;
+	void Unbind() const;
+
+	// Set uniforms
+	void SetUniform1i(const std::string& name, int v0);
+
+	void SetUniform1f(const std::string& name, float v0);
+	void SetUniform2f(const std::string& name, float v0, float v1);
+	void SetUniform3f(const std::string& name, float v0, float v1, float v2);
+	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+
+private:
+	unsigned int CompileShader(unsigned int type, const std::string& source);
+	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+	ShaderProgramSource ParseShader(const std::string& filepath);
+	int GetUniformLocation(const std::string& name);
+};
